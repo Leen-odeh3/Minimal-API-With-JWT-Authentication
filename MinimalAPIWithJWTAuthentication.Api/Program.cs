e1ccc6f9a4f3a1ac224e0a38d56abef38f693a27
Leen Odeh <123558998+Leen-odeh3@users.noreply.github.com>
2024-06-11 04:08:16 +0300

    add InMemoryUserRepository

--- MinimalAPIWithJWTAuthentication.Api/Program.cs
+++ MinimalAPIWithJWTAuthentication.Api/Program.cs
@@ -1,70 +1,61 @@
+using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using MinimalAPIWithJWTAuthentication.API.Repositories;
 using MinimalAPIWithJWTAuthentication.Api.Abstracts;
 using MinimalAPIWithJWTAuthentication.Api.Configurations;
 using MinimalAPIWithJWTAuthentication.Api.Services;
-using System.Text;
 
-namespace MinimalAPIWithJWTAuthentication.Api
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+  .Configure<JwtAuthenticationConfig>(builder.Configuration.GetSection(nameof(JwtAuthenticationConfig)))
+  .AddScoped<IUserRepo, InMemoryUserRepository>()
+  .AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
+
+builder.Services.AddControllers();
+
+builder.Services.AddAuthentication(options =>
 {
-    public class Program
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    var config = builder.Configuration.GetSection(nameof(JwtAuthenticationConfig)).Get<JwtAuthenticationConfig>()!;
+
+    var key = Encoding.UTF8.GetBytes(config.Key);
+
+    options.TokenValidationParameters = new TokenValidationParameters
     {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = config.Issuer,
+        ValidAudience = config.Audience,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ClockSkew = TimeSpan.Zero
+    };
+});
 
-            // Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
-            builder.Services
-          .Configure<JwtAuthenticationConfig>(builder.Configuration.GetSection(nameof(JwtAuthenticationConfig)))
-          .AddScoped<IUserRepo, InMemoryUserRepository>()
-          .AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
+var app = builder.Build();
 
-            builder.Services.AddControllers();
-
-
-            builder.Services.AddAuthentication(options =>
-            {
-                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-            }).AddJwtBearer(options =>
-            {
-                var config = builder.Configuration.GetSection(nameof(JwtAuthenticationConfig)).Get<JwtAuthenticationConfig>()!;
-
-                var key = Encoding.UTF8.GetBytes(config.Key);
-
-                options.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuer = true,
-                    ValidateAudience = true,
-                    ValidateLifetime = true,
-                    ValidateIssuerSigningKey = true,
-                    ValidIssuer = config.Issuer,
-                    ValidAudience = config.Audience,
-                    IssuerSigningKey = new SymmetricSecurityKey(key),
-                    ClockSkew = TimeSpan.Zero
-                };
-            });
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseAuthentication();
-            app.UseAuthorization();
-            app.MapControllers();
-
-            app.Run();
-        }
-    }
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ MinimalAPIWithJWTAuthentication.Api/Repositories/InMemoryUserRepository.cs
@@ -0,0 +1,32 @@
+﻿using MinimalAPIWithJWTAuthentication.Api.Abstracts;
+using MinimalAPIWithJWTAuthentication.Api.Models;
+
+namespace MinimalAPIWithJWTAuthentication.Api.Repositories;
+
+public class InMemoryUserRepository : IUserRepo
+{
+       private readonly List<User> _users = new()
+       {
+            new User
+            {
+              UserID = 1,
+              FirstName = "leen",
+              LastName = "odeh",
+              Username = "leen_3",
+              Password = "test1"
+            },
+            new User
+            {
+              UserID = 2,
+              FirstName = "sara",
+              LastName = "mohammad",
+              Username = "sara221",
+              Password = "test2"
+            }
+       };
+
+        public Task<User?> Get(string username, string password)
+        {
+            return Task.FromResult(_users.FirstOrDefault(u => username == u.Username && password == u.Password));
+        }
+}
