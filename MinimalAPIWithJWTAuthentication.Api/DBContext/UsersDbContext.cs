4806d92f7d27d7020e38cd3ca7c3faa39ba712a6
Leen Odeh <123558998+Leen-odeh3@users.noreply.github.com>
2024-06-11 12:09:40 +0300

    Create Db Context

---
+++ MinimalAPIWithJWTAuthentication.Api/DBContext/UsersDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinimalAPIWithJWTAuthentication.Api.Models;
+
+namespace MinimalAPIWithJWTAuthentication.Api.DBContext;
+
+public class UsersDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+
+    public UsersDbContext(DbContextOptions<UsersDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Seed();
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
