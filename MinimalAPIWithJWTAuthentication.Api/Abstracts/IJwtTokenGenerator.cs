eecaec451ca13b884d419ce2b8959749def9cda0
Leen Odeh <123558998+Leen-odeh3@users.noreply.github.com>
2024-06-11 03:52:38 +0300

    Add JwtAuthenticationConfig

---
+++ MinimalAPIWithJWTAuthentication.Api/Abstracts/IJwtTokenGenerator.cs
@@ -0,0 +1,9 @@
+﻿using MinimalAPIWithJWTAuthentication.Api.Models;
+
+namespace MinimalAPIWithJWTAuthentication.Api.Abstracts;
+public interface IJwtTokenGenerator
+{
+    string GenerateToken(User user);
+
+    bool ValidateToken(string token);
+}
---
+++ MinimalAPIWithJWTAuthentication.Api/Configurations/JwtAuthenticationConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace MinimalAPIWithJWTAuthentication.Api.Configurations;
+public class JwtAuthenticationConfig
+{
+    public string Key { get; set; } = string.Empty;
+
+    public string Issuer { get; set; } = string.Empty;
+
+    public string Audience { get; set; } = string.Empty;
+
+    public double LifetimeMinutes { get; set; }
+}
