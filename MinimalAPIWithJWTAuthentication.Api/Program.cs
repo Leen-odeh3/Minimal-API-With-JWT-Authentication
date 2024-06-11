d5abdd3427c603da3039d5997ff6a1521a0e006a
Leen Odeh <123558998+Leen-odeh3@users.noreply.github.com>
2024-06-11 04:03:36 +0300

    Configure dependency injection and authentication in the app entry point

--- MinimalAPIWithJWTAuthentication.Api/Program.cs
+++ MinimalAPIWithJWTAuthentication.Api/Program.cs
@@ -1,3 +1,10 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using MinimalAPIWithJWTAuthentication.Api.Abstracts;
+using MinimalAPIWithJWTAuthentication.Api.Configurations;
+using MinimalAPIWithJWTAuthentication.Api.Services;
+using System.Text;
+
 namespace MinimalAPIWithJWTAuthentication.Api
 {
     public class Program
@@ -8,7 +15,37 @@ namespace MinimalAPIWithJWTAuthentication.Api
 
             // Add services to the container.
 
+            builder.Services
+          .Configure<JwtAuthenticationConfig>(builder.Configuration.GetSection(nameof(JwtAuthenticationConfig)))
+          .AddScoped<IUserRepo, InMemoryUserRepository>()
+          .AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
+
             builder.Services.AddControllers();
+
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                var config = builder.Configuration.GetSection(nameof(JwtAuthenticationConfig)).Get<JwtAuthenticationConfig>()!;
+
+                var key = Encoding.UTF8.GetBytes(config.Key);
+
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = config.Issuer,
+                    ValidAudience = config.Audience,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
@@ -23,10 +60,8 @@ namespace MinimalAPIWithJWTAuthentication.Api
             }
 
             app.UseHttpsRedirection();
-
+            app.UseAuthentication();
             app.UseAuthorization();
-
-
             app.MapControllers();
 
             app.Run();
