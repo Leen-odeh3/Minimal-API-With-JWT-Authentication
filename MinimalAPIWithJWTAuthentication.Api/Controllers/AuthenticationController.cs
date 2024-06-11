232ac12c467537a5ba46ff8b9bff116f5e36e434
Leen Odeh <123558998+Leen-odeh3@users.noreply.github.com>
2024-06-11 03:59:19 +0300

    " Return message "

--- MinimalAPIWithJWTAuthentication.Api/Controllers/AuthenticationController.cs
+++ MinimalAPIWithJWTAuthentication.Api/Controllers/AuthenticationController.cs
@@ -1,6 +1,28 @@
-﻿namespace MinimalAPIWithJWTAuthentication.Api.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using MinimalAPIWithJWTAuthentication.Api.Abstracts;
+
+namespace MinimalAPIWithJWTAuthentication.Api.Controllers;
+
+public class AuthenticationController
 {
-    public class AuthenticationController
+    private readonly IJwtTokenGenerator _jwtTokenGenerator;
+
+    private readonly IUserRepo _userRepository;
+
+    public AuthenticationController(IUserRepo userRepository, IJwtTokenGenerator jwtTokenGenerator)
     {
+        _userRepository = userRepository ?? throw new ArgumentNullException(nameof(userRepository));
+
+        _jwtTokenGenerator = jwtTokenGenerator ?? throw new ArgumentNullException(nameof(jwtTokenGenerator));
+    }
+
+    [HttpPost("authenticate")]
+    public async Task<ActionResult<string>> Authenticate(AuthRequestBody authenticationRequestBody)
+    {
+        var user = await _userRepository.Get(authenticationRequestBody.Username, authenticationRequestBody.Password);
+
+        if (user is null) return Unauthorized();
+
+        return Ok(_jwtTokenGenerator.GenerateToken(user));
     }
 }
---
+++ MinimalAPIWithJWTAuthentication.Api/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MinimalAPIWithJWTAuthentication.Api.Controllers;
+public class TestController
+{
+    [HttpGet]
+    public ActionResult<string> GetMessage()
+    {
+        return Ok("Welcome to our API! Thank you for visiting.");
+    }
+
+}
