258bfdc96b409791689dd088e7d2800544e6398a
Leen Odeh <123558998+Leen-odeh3@users.noreply.github.com>
2024-06-11 12:19:04 +0300

    add connectionString and dependancy at startup class

--- MinimalAPIWithJWTAuthentication.Api/Program.cs
+++ MinimalAPIWithJWTAuthentication.Api/Program.cs
@@ -5,39 +5,42 @@ using MinimalAPIWithJWTAuthentication.Api.Abstracts;
 using MinimalAPIWithJWTAuthentication.Api.Configurations;
 using MinimalAPIWithJWTAuthentication.Api.Services;
 using MinimalAPIWithJWTAuthentication.Api.Repositories;
+using MinimalAPIWithJWTAuthentication.Api.DBContext;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services
-  .Configure<JwtAuthenticationConfig>(builder.Configuration.GetSection(nameof(JwtAuthenticationConfig)))
-  .AddScoped<IUserRepo, InMemoryUserRepository>()
-  .AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
 
 builder.Services.AddControllers();
 
-builder.Services.AddAuthentication(options =>
+builder.Services.AddDbContext<UsersDbContext>(options =>
 {
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(options =>
-{
-    var config = builder.Configuration.GetSection(nameof(JwtAuthenticationConfig)).Get<JwtAuthenticationConfig>()!;
-
-    var key = Encoding.UTF8.GetBytes(config.Key);
-
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-        ValidIssuer = config.Issuer,
-        ValidAudience = config.Audience,
-        IssuerSigningKey = new SymmetricSecurityKey(key),
-        ClockSkew = TimeSpan.Zero
-    };
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
 });
+
+builder.Services.AddScoped<IUserRepo, UserRepo>();
+builder.Services.AddScoped<UserService>();
+builder.Services.AddScoped<ITokenService, JwtTokenGenerator>();
+builder.Services.Configure<JwtAuthenticationConfig>(builder.Configuration.GetSection("JWTToken"));
+
+var jwtConfig = builder.Configuration.GetSection("JWTToken").Get<JwtAuthenticationConfig>() ?? throw new ArgumentNullException("JWTToken:Wrong Configuration");
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        var key = new SymmetricSecurityKey(Convert.FromBase64String(jwtConfig.SecretKey));
+
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = jwtConfig.Issuer,
+            ValidAudience = jwtConfig.Audience,
+            IssuerSigningKey = key
+        };
+    });
+
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
